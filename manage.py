19924ddece18dd736e5cbbe4aad85d10a4d09cc6
Harish <hareeshankar@gmail.com>
2018-09-19 20:25:37 +0530

    removed super user create

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-from django.contrib.auth import get_user_model; 
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
@@ -9,4 +8,4 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-    User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password1')
+
