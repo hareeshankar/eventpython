1ebf4522451dc79def6768c66bcab1220bcf40db
Harish <hareeshankar@gmail.com>
2018-09-19 20:14:25 +0530

    super user creation

--- manage.py
+++ manage.py
@@ -3,11 +3,10 @@ import os
 import sys
 from django.contrib.auth import get_user_model; 
 
-User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'my secure password')
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+    User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password1')
